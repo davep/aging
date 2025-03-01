00d7ad375a40176712e9f9e9543e692ed72651ed
Dave Pearson <davep@davep.org>
2025-03-01 16:34:32 +0000

    :sparkles: Add the bare scaffold for the main screen

--- src/aging/aging.py
+++ src/aging/aging.py
@@ -7,6 +7,8 @@ from textual_enhanced.app import EnhancedApp
 ##############################################################################
 # Local imports.
 from . import __version__
+from .screens import Main
+
 
 ##############################################################################
 class Aging(EnhancedApp[None]):
@@ -15,5 +17,10 @@ class Aging(EnhancedApp[None]):
     HELP_TITLE = f"Aging {__version__}"
     HELP_ABOUT = "There will be more here."
     HELP_LICENSE = "License information will go here."
+
+    def get_default_screen(self) -> Main:
+        """Get the main screen for the application."""
+        return Main()
+
 
 ### aging.py ends here
---
+++ src/aging/screens/main.py
@@ -0,0 +1,46 @@
+"""Provides the main screen."""
+
+##############################################################################
+# Textual imports.
+from textual.app import ComposeResult
+from textual.widgets import Footer, Header
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.commands import ChangeTheme, Command, Help, Quit
+from textual_enhanced.screen import EnhancedScreen
+
+##############################################################################
+# Local imports.
+from .. import __version__
+
+
+##############################################################################
+class Main(EnhancedScreen[None]):
+    """The main screen for the application."""
+
+    TITLE = f"Aging v{__version__}"
+
+    HELP = """
+    ## Main application keys and commands
+
+    The following key bindings and commands are available:
+    """
+
+    COMMAND_MESSAGES = (
+        # Keep these together as they're bound to function keys and destined
+        # for the footer.
+        Help,
+        ChangeTheme,
+        Quit,
+    )
+
+    BINDINGS = Command.bindings(*COMMAND_MESSAGES)
+
+    def compose(self) -> ComposeResult:
+        """Compose the content of the main screen."""
+        yield Header()
+        yield Footer()
+
+
+### main.py ends here
