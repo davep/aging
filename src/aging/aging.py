7d2de4d27eabfdc86ed6b785ca108ead07b7ef63
Dave Pearson <davep@davep.org>
2025-03-04 08:03:42 +0000

    :sparkles: Make the theme sticky

--- src/aging/aging.py
+++ src/aging/aging.py
@@ -1,12 +1,20 @@
 """The main application class."""
 
 ##############################################################################
+# Textual imports.
+from textual.app import InvalidThemeError
+
+##############################################################################
 # Textual enhanced imports.
 from textual_enhanced.app import EnhancedApp
 
 ##############################################################################
 # Local imports.
 from . import __version__
+from .data import (
+    load_configuration,
+    update_configuration,
+)
 from .screens import Main
 
 
@@ -19,6 +27,25 @@ class Aging(EnhancedApp[None]):
     HELP_LICENSE = "License information will go here."
 
     COMMANDS = set()
+
+    def __init__(self) -> None:
+        """Initialise the application.
+
+        Args:
+            The command line arguments passed to the application.
+        """
+        super().__init__()
+        configuration = load_configuration()
+        if configuration.theme is not None:
+            try:
+                self.theme = configuration.theme
+            except InvalidThemeError:
+                pass
+
+    def watch_theme(self) -> None:
+        """Save the application's theme when it's changed."""
+        with update_configuration() as config:
+            config.theme = self.theme
 
     def get_default_screen(self) -> Main:
         """Get the main screen for the application."""
