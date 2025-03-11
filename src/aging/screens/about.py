4930bd820c3cc5dcc22dbad82a9cbedd882d71c6
Dave Pearson <davep@davep.org>
2025-03-11 08:37:51 +0000

    :sparkles: Add more information to the About dialog

--- src/aging/screens/about.py
+++ src/aging/screens/about.py
@@ -1,15 +1,46 @@
 """Provides a dialog for showing information about a guide."""
 
 ##############################################################################
+# Python imports.
+from datetime import datetime
+
+##############################################################################
+# Humanize imports.
+from humanize import naturalsize
+
+##############################################################################
 # NGDB imports.
 from ngdb import NortonGuide, make_dos_like
 
 ##############################################################################
 # Textual imports.
+from textual import on
 from textual.app import ComposeResult
-from textual.containers import Vertical
+from textual.containers import Horizontal, Vertical
 from textual.screen import ModalScreen
-from textual.widgets import Label
+from textual.widgets import Button, Label
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.tools import add_key
+
+
+##############################################################################
+class Title(Label):
+    """A widget to show a title."""
+
+    DEFAULT_CSS = """
+    Title {
+        text-style: bold;
+        color: $accent;
+        width: 100%;
+    }
+    """
+
+
+##############################################################################
+class Data(Label):
+    """A widget to show data."""
 
 
 ##############################################################################
@@ -24,6 +55,28 @@ class About(ModalScreen[None]):
             height: auto;
             background: $panel;
             border: solid $border;
+        }
+
+        #buttons {
+            align: center middle;
+            border-top: solid $border;
+            width: 100%;
+            height: auto;
+        }
+
+        Title, {
+            margin: 1 2 0 2;
+        }
+        Data {
+            margin: 0 2 1 2;
+        }
+
+        #credits {
+            margin: 1 2 1 2;
+            border-title-align: right;
+            border-title-color: $accent;
+            border: solid $border;
+            background: $secondary;
         }
     }
     """
@@ -44,7 +97,31 @@ class About(ModalScreen[None]):
         """Compose the content of the screen."""
         with Vertical() as dialog:
             dialog.border_title = f"About {self._guide.path.name}"
-            yield Label("\n".join(make_dos_like(line) for line in self._guide.credits))
+            if "".join(line.strip() for line in self._guide.credits):
+                yield (
+                    data := Data(
+                        "\n".join(make_dos_like(line) for line in self._guide.credits),
+                        id="credits",
+                    )
+                )
+                data.border_title = "Credits"
+            yield Title("Made With:")
+            yield Data(self._guide.made_with)
+            yield Title("File:")
+            yield Data(str(self._guide.path))
+            yield Title("Size:")
+            yield Data(naturalsize(self._guide.path.stat().st_size))
+            yield Title("Created:")
+            yield Data(
+                f"{datetime.fromtimestamp(int(self._guide.path.stat().st_ctime))}"
+            )
+            with Horizontal(id="buttons"):
+                yield Button(add_key("Close", "Esc", self))
+
+    @on(Button.Pressed)
+    def _close_about(self) -> None:
+        """Close the about dialog."""
+        self.dismiss(None)
 
 
 ### about.py ends here
