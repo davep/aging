911b00f1be3ba5d2ca78bfb239f336e65e67a482
Dave Pearson <davep@davep.org>
2025-03-17 07:15:44 +0000

    :construction: WIP: Guide-wide and directory-wide search (redux)
    More work on getting the global search up and running. This mostly involves
    more UI work and also code to actually walk through the guides and their
    entries.
    
    This was a worthwhile task too as I managed to uncover a couple of bugs (or
    at least things that need taking into account) in ngdb.py:
    
      https://github.com/davep/ngdb.py/issues/17
      https://github.com/davep/ngdb.py/issues/18

--- src/aging/screens/search.py
+++ src/aging/screens/search.py
@@ -3,8 +3,11 @@
 ##############################################################################
 # NGDB imports.
 from dataclasses import dataclass
+from typing import Iterator
 
-from ngdb import NortonGuide
+##############################################################################
+# NGDB imports
+from ngdb import Long, NGDBError, NortonGuide, PlainText, Short, make_dos_like
 
 ##############################################################################
 # Textual imports.
@@ -14,7 +17,7 @@ from textual.containers import HorizontalGroup, VerticalGroup
 from textual.message import Message
 from textual.screen import ModalScreen
 from textual.widgets import Button, Checkbox, Input, Label, ProgressBar
-from textual.worker import get_current_worker
+from textual.worker import Worker, get_current_worker
 
 ##############################################################################
 # Local imports.
@@ -90,8 +93,10 @@ class Search(ModalScreen[None]):
                 yield Button(
                     "Stop", variant="error", id="stop", classes="--when-running"
                 )
-            yield Label(id="current_guide", classes="--when-running")
+            yield Label(id="current_guide", classes="--when-running", markup=False)
             yield ProgressBar(id="guides_progress", classes="--when-running")
+            yield Label(id="current_entry", classes="--when-running", markup=False)
+            yield ProgressBar(id="guide_progress", classes="--when-running")
 
     @dataclass
     class Started(Message):
@@ -99,6 +104,12 @@ class Search(ModalScreen[None]):
 
         searching: int
         """The number of guides we'll be searching."""
+
+    class Ended(Message):
+        """Message sent when the search has ended."""
+
+    class Cancelled(Message):
+        """Message sent when the search has been cancelled."""
 
     @dataclass
     class NewGuide(Message):
@@ -109,23 +120,161 @@ class Search(ModalScreen[None]):
         guide: Guide
         """The information about the guide being searched."""
 
+    @dataclass
+    class NewEntry(Message):
+        """Message sent when a new entry is being searched."""
+
+        guide: NortonGuide
+        """The guide being searched."""
+        entry: Short | Long
+        """The entry being searched."""
+
+    class FinishedGuide(Message):
+        """Message sent when we've finished searching a guide."""
+
     @on(Started)
     def _search_started(self, starting: Started) -> None:
+        """Handle a search starting.
+
+        Args:
+            starting: The message that signals that a search has started.
+        """
+        for widget in self.query("Input, Checkbox"):
+            widget.disabled = True
         self.set_class(True, "--running")
         self.query_one("#guides_progress", ProgressBar).total = starting.searching
 
+    @on(Ended)
+    @on(Cancelled)
+    def _search_ended(self) -> None:
+        for widget in self.query("Input, Checkbox"):
+            widget.disabled = False
+        self.set_class(False, "--running")
+        """Handle the search ending."""
+
     @on(NewGuide)
     def _update_current_guide(self, current: NewGuide) -> None:
+        """Handle the a new guide being searched.
+
+        Args:
+            current: The message that signals that a new guide is being searched.
+        """
         self.query_one("#current_guide", Label).update(
             f"Searching {current.guide.title}"
         )
         self.query_one("#guides_progress", ProgressBar).progress = current.position
+        self.query_one(
+            "#guide_progress", ProgressBar
+        ).total = current.guide.location.stat().st_size
+
+    def _entry_description(
+        self, guide: NortonGuide, entry: Short | Long
+    ) -> Iterator[str]:
+        """Generate a description for the given entry.
+
+        Args:
+            guide: The guide that is being searched.
+            entry: The entry that is being searched.
+
+        Yields:
+            Parts of a description for the entry.
+        """
+        yield entry.__class__.__name__
+        if entry.parent.has_menu:
+            yield make_dos_like(guide.menus[entry.parent.menu].title)
+        if entry.parent.has_prompt:
+            yield make_dos_like(
+                guide.menus[entry.parent.menu].prompts[entry.parent.prompt]
+            )
+        if first_non_empty_line := (
+            next((line for line in entry if line.strip()), "")
+            if isinstance(entry, Long)
+            else next((line.text for line in entry if line.text.strip()), "")
+        ):
+            yield make_dos_like(str(PlainText(first_non_empty_line)))
+
+    @on(NewEntry)
+    def _update_current_entry(self, current: NewEntry) -> None:
+        """Handle a new entry being searched.
+
+        Args:
+            current: The message that signals a new entry is being searched.
+        """
+        try:
+            self.query_one("#current_entry", Label).update(
+                " » ".join(self._entry_description(current.guide, current.entry))
+            )
+            self.query_one(
+                "#guide_progress", ProgressBar
+            ).progress = current.entry.offset
+        except ValueError:
+            # TODO: REMOVE!
+            self.notify("Value error found")
+
+    @on(FinishedGuide)
+    def _finished_guide(self) -> None:
+        """Handle a guide search finishing."""
+        self.query_one("#current_entry", Label).update("Finished")
+        if (total := self.query_one("#guide_progress", ProgressBar).total) is not None:
+            self.query_one("#guide_progress", ProgressBar).progress = total
+
+    def _search_guide(
+        self, guide: Guide, worker: Worker, needle: str, ignore_case: bool
+    ) -> None:
+        """Search within the given guide.
+
+        Args:
+            guide: The guide being searched.
+            worker: The worker that we're working within.
+            needle: The text to search for.
+            ignore_case: Should case be ignored?
+        """
+        try:
+            with NortonGuide(guide.location) as search:
+                for entry in search:
+                    if worker.is_cancelled:
+                        return
+                    self.post_message(self.NewEntry(search, entry))
+            self.post_message(self.FinishedGuide())
+        except (IOError, NGDBError) as error:
+            self.notify(
+                str(error), title=f"Failed to search {guide.location}", severity="error"
+            )
+        except IndexError:
+            # TODO: REMOVE!
+            self.notify("Index error found")
+
+    @work(thread=True, exclusive=True, group="search")
+    def _search(self, guides: Guides, needle: str, ignore_case: bool) -> None:
+        """Start a new search.
+
+        Args:
+            guides: The guides to search.
+            needle: The text to search for.
+            ignore_case: Should case be ignored?
+        """
+        worker = get_current_worker()
+        needle = needle.casefold() if ignore_case else needle
+        self.post_message(self.Started(len(guides)))
+        for position, guide in enumerate(sorted(guides)):
+            if worker.is_cancelled:
+                self.post_message(self.Cancelled())
+                return
+            self.post_message(self.NewGuide(position + 1, guide))
+            self._search_guide(guide, worker, needle, ignore_case)
+        self.post_message(self.Ended())
 
     @on(Input.Submitted)
     @on(Button.Pressed, "#go")
-    @work(thread=True, exclusive=True)
-    def _start_search(self) -> None:
-        """Start a new search."""
+    def search(self) -> None:
+        """React to a request to start a search."""
+        if not self.query_one(Input).value.strip():
+            self.notify(
+                "Please provide something to search for",
+                title="No search text",
+                severity="error",
+            )
+            return
         guides = self._guides
         if not self.query_one("#global", Checkbox).value:
             if self._guide is None:
@@ -136,15 +285,16 @@ class Search(ModalScreen[None]):
                 )
                 return
             guides = [Guide(self._guide.title, self._guide.path)]
-        worker = get_current_worker()
-        self.post_message(self.Started(len(guides)))
-        from time import sleep
+        self._search(
+            guides,
+            self.query_one(Input).value,
+            self.query_one("#ignore_case", Checkbox).value,
+        )
 
-        for position, guide in enumerate(sorted(guides)):
-            if worker.is_cancelled:
-                return
-            self.post_message(self.NewGuide(position + 1, guide))
-            sleep(0.05)
+    @on(Button.Pressed, "#stop")
+    def stop_search(self) -> None:
+        """Stop the search."""
+        self.app.workers.cancel_group(self, "search")
 
 
 ### search.py ends here
